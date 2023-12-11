77099a434f1c326c02e0f4534853556c3181a493
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2023-12-11 17:34:06 +0100

    cleanup

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { default as VtBotDebug } from './src/VtBotDebug.astro';
-export { default as ReplacementSwap } from './src/ReplacementSwap.astro';
-export { default as Linter } from './src/Linter.astro';
