01c35c5ccc8cbcdec0631fcc6594ee336d1f28ef
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2023-12-26 21:15:47 +0100

    Debug github actions

--- example/bin/relocate.sh
+++ example/bin/relocate.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 BASE=${1:-"/"}
-find dist -type f -name '*.html' -exec sed -i -e "s^\(\(href\|src\|srcset\)\)=\"/^\1=\"${BASE}^" -e "s^content=\"/carousel/^content=\"${BASE}carousel/^" -e "s^:/carousel/^:${BASE}carousel/^" {} \; 
+find dist -type f -name '*.html' -exec sed -i -e "s^\(\(href\|src\|srcset\)\)=\"/^\1=\"${BASE}^" -e "s^content=\"/carousel/^content=\"${BASE}carousel/^" -e "s^:/carousel/^:${BASE}carousel/^" {} \; -print
 
+cat dist/loading/two/index.html
