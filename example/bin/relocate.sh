fe4dcfb2b91f652bd63931c4c63f322877dab3d3
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2023-12-28 20:24:22 +0100

    Improve build

--- example/bin/relocate.sh
+++ example/bin/relocate.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 
 BASE=${1:-"/"}
-find dist -type f -name '*.html' -exec sed -i -e "s^\(\(href\|src\|srcset\)\)=\"/^\1=\"${BASE}^g" -e "s^content=\"/carousel/^content=\"${BASE}carousel/^g" -e "s^:/carousel/^:${BASE}carousel/^g" {} \; -print
+echo "Relocating '/' to '${BASE}'"
+find dist -type f \( -name "*.html" -o -name "*.js" \) -exec sed -i \
+-e "s^url(/_astro/^url(${BASE}_astro/^g" \
+-e "s^\(\(href\|src\|srcset\)\)=\"/^\1=\"${BASE}^g" \
+-e "s^content=\"/carousel/^content=\"${BASE}carousel/^g" \
+-e "s^:/carousel/^:${BASE}carousel/^g" {} \; -print
