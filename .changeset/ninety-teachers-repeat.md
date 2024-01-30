502e89bb67e87a36742450d2d810e57bcb479a72
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-01-30 16:29:06 +0100

    show scripts in VtBotDebug (#15)
    * show scripts in VtBotDebug
    
    * Update ninety-teachers-repeat.md

---
+++ .changeset/ninety-teachers-repeat.md
@@ -0,0 +1,5 @@
+---
+"astro-vtbot": patch
+---
+
+`<VtBotDebug />` now shows which scripts are executed and which aren't between `astro:after-swap` & `astro:page-load` events
--- components/css.ts
+++ components/css.ts
@@ -1,30 +1,6 @@
 // todos:
 // check for different CSS rule types (beside CSSStyleRule)
 
-export function deriveCSSSelector(element: Element, useIds = true) {
-	let path: string[] = [];
-	while (element && element.nodeType === Node.ELEMENT_NODE) {
-		let selector = element.nodeName.toLowerCase();
-		if (useIds && element.id) {
-			selector = '#' + element.id;
-			path.unshift(selector);
-			break;
-		} else {
-			let sibling = element;
-			let nth = 1;
-			while ((sibling = sibling.previousElementSibling as Element)) {
-				if (sibling.nodeName.toLowerCase() === selector) nth++;
-			}
-			if (nth !== 1) {
-				selector += ':nth-of-type(' + nth + ')';
-			}
-		}
-		path.unshift(selector);
-		element = element.parentNode as Element;
-	}
-	return path.join(' > ');
-}
-
 export function astroContextIds() {
 	const inStyleSheets = new Set<string>();
 	const inElements = new Set<string>();
