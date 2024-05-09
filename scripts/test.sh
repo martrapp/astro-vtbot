fb977e50a926efb768f0605c7d4952f11b7020d7
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-05-09 21:44:36 +0200

    watch for Atro changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-
+diff -q components/swap-functions.ts node_modules/astro/src/transitions 2>&1 > /dev/null
+if [ $? -ne 0 ] ; then
+	cp node_modules/astro/src/transitions/swap-functions.ts components
+	echo "components/swap-functions.ts updated"
+fi
+exit
 if [ "$GITHUB_ACTIONS" = "true" ]; then
 	npx playwright install chromium
 fi
