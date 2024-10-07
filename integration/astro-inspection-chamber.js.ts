9a094500c75011c82667bc9de2a061715242351e
Lukas Trombach <me@lukastrombach.dev>
2024-10-07 16:49:57 +0200

    use `raw` import for `inspectionChamber` endpoint

--- integration/astro-inspection-chamber.js.ts
+++ integration/astro-inspection-chamber.js.ts
@@ -1,6 +1,4 @@
-import { readFileSync } from 'node:fs';
-
-const inspectionChamber = readFileSync('node_modules/@vtbag/inspection-chamber/lib/index.js');
+import inspectionChamber from '@vtbag/inspection-chamber/lib/index.js?raw';
 
 export async function GET({ params, request }) {
 	return new Response(
