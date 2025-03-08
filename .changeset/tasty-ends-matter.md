352adea34087c5b8a5a6791b432d4eed7b483a15
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-03-08 16:30:07 +0100

    Remove own swap-functions in favor of Astro's API

---
+++ .changeset/tasty-ends-matter.md
@@ -0,0 +1,5 @@
+---
+'astro-vtbot': patch
+---
+
+Replaces swap-function support with Astro's official implementation for better compatibility.
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,6 @@
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
 	npx playwright install chromium
-else
-	diff -q components/swap-functions.js node_modules/astro/dist/transitions 2>&1 > /dev/null
-	if [ $? -ne 0 ] ; then
-		cp node_modules/astro/dist/transitions/swap-functions.js components
-		echo "components/swap-functions.js updated"
-	fi
 fi
 mkdir -p test/fixture/node_modules
 (
